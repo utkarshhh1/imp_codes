23c25992e584bcb66b49e670bf658495ff1639ed
Astra <117833599+utkarshhh1@users.noreply.github.com>
2023-04-05 22:37:16 +0530

    Add files via upload
    Pyramid pattern in c++

---
+++ 180_pyramid.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n ,i,j;
+cout<<"enter a number ";
+cin>>n;
+for(i=0;i<=n;i++)
+{
+  for(j=1;j<=n;j++)
+  {
+    if(j<=n-i)
+    {
+        cout<<" ";
+    }
+    else
+  {cout<<"*";}
+
+
+  }
+  
+
+
+cout<<endl;
+}
+return 0;
+}
